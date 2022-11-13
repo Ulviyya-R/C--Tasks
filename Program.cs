bdc3a6571b1bf05c03128cb1c61a640f75e1df7d
Ulviyya-R <ulviyyamr@code.edu.az>
2022-11-14 00:16:29 +0400

    These are c# tasks

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿#region 2 nin quvveti 
+////Console.WriteLine("Please choose number");
+////string numStr = Console.ReadLine();
+////int num = Convert.ToInt32(numStr);
+
+
+
+////{
+////    while (num % 2 == 0)
+////    { 
+////        num = num / 2;
+////}
+////if (num == 1)
+////{
+////    Console.WriteLine("Quvvetidir");
+////}
+////else
+////{
+////    Console.WriteLine("Quvveti deyil");
+////}
+////}
+#endregion
+
+
+#region 70 den boyuk ve ya kicikdirse
+//using System.Runtime.CompilerServices;
+
+//Console.WriteLine("Please choose number: ");
+//string numStr = Console.ReadLine();
+//int num1 = Convert.ToInt32(numStr);
+
+//if (num1 < 70)
+//{
+//    num1 = 3 * (70 - num1);
+//    Console.WriteLine(num1);
+//}
+//else
+//{
+//    num1 -= 70;
+//    Console.WriteLine(num1);
+//}
+#endregion
+
+#region X , Y , Z en boyuk
+//Console.WriteLine("X : ");
+//string num1 = Console.ReadLine();
+//Console.WriteLine("Y : ");
+//string num2 = Console.ReadLine();
+//Console.WriteLine("Z :");
+//string num3 = Console.ReadLine();
+//int numx = Convert.ToInt32(num1);
+//int numy = Convert.ToInt32(num2);
+//int numz = Convert.ToInt32(num3);
+
+//if (numx > numy && numx > numz)
+//{
+//    Console.WriteLine("En boyuk eded X: " + numx);
+//}
+//else if (numy > numx && numy > numz)
+//{
+//    Console.WriteLine("En boyuk eded Y: "  + numy);
+//}
+//else if (numz > numx && numz > numy)
+//{
+//    Console.WriteLine("En boyuk eded Z:  "  + numz);
+//}
+#endregion
+
+
+#region 2 eded arasindaki ededler
+//Console.WriteLine(" 1ci ededi daxil edin: ");
+//string num1 = Console.ReadLine();
+//Console.WriteLine(" 2 ci ededi daxil edin: ");
+//string num2 = Console.ReadLine();
+
+//int numx = Convert.ToInt32(num1);
+//int numy = Convert.ToInt32(num2);
+
+//for (int i = numx; i <= numy ; i++)
+
+//{
+//    Console.WriteLine(i);
+//}
+#endregion
+
+
+#region N ve M ededleri arasinda tek ededlerin sayi
+//Console.WriteLine("N : ");
+//string nn = Console.ReadLine();
+//Console.WriteLine("M : ");
+//string mm = Console.ReadLine();
+
+
+//int n = Convert.ToInt32(nn);
+//int m = Convert.ToInt32(mm);
+//int count = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+
+//    }
+
+
+//    else
+//    {
+//         count++;
+
+//    }
+
+//}
+//Console.WriteLine("Tek ededlerin sayi :   "  + count);
+#endregion
+
+
+
